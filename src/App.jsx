e6b0e688633b390d7f2cdeb2ab6d56ee3ec7fe9b
Biro <luizhenriquegoncalvessilva@hotmail.com>
2023-05-28 22:44:20 -0300

    Updated App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Grid, Typography } from "@mui/material";
+
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
-
   return (
     <>
-      
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ gap: "112px" }}
+      >
+        <Header />
+        <Hero />
+        <Grid
+          lg={12}
+         
+          style={{ background: "#653DF5"}}
+        >
+          <Grid style={{padding: "24px 0 24px 0" }}  display={"flex"} alignContent={"center"}  justifyContent={"center"} gap={16}   >
+            <span>FrontEnd</span>
+            <span>Desenvolvimento</span>
+            <span>Especialista</span>
+            <span>Code</span>
+            <span>Design</span>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
