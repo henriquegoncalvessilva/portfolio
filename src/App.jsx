a182d98543f30839dc072de485f8de20eb430b94
Biro <luizhenriquegoncalvessilva@hotmail.com>
2023-05-31 22:33:32 -0300

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import "./App.css";
-import { Grid } from "@mui/material";
+import { Grid, Box, Typography } from "@mui/material";
 
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
 import Differentials from "./components/Differentials/Differentials";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
   return (
     <>
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="center"
-      >
+      <Grid container alignItems="center" justifyContent="center">
         <Header />
         <Hero />
-        <Skills/>
-        <Differentials/>
+        <Skills />
+        <Differentials />
+        <AboutMe/>
+       
       </Grid>
     </>
   );
--- src/components/Differentials/Differentials.jsx
+++ src/components/Differentials/Differentials.jsx
@@ -43,7 +43,7 @@ const Differentials = () => {
         
       </div>
     </section>
-  </Grid>
+    </Grid>
   )
 }
 
